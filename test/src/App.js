8e19de832a646f73af6f9f5502e41034ca634f67
gwangmiiin <0424kgm@gmail.com>
2022-04-21 20:41:42 +0900

    useState를 state로 변경

--- test/src/App.js
+++ test/src/App.js
@@ -3,11 +3,13 @@ import Blog from './components/Blog';
 //import Box from './components/Box'
 //import Mount from './components/Mount';
 //import Userlist from './components/Userlist';
+import Say from './components/Say';
+import Say2 from './components/Say2';
 
 function App() {
   return (
     <div className="App">
-      <Blog />
+      <Say2 />
     </div>
   );
 }
---
+++ test/src/components/Say2.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Say2 extends Component {
+    state = {
+      message : '초기값',
+      color : {color : 'blue'}
+    }
+    handleColor = () => {
+      this.setState({
+        color :{color : 'red'}
+      })
+    }
+    onClickEnter = () => {
+      this.setState({
+        message : '입장'
+      })
+    }
+    onClickLeave = () => {
+      this.setState({
+        message : '퇴장'
+      })
+    }
+    render(){
+    return (
+      <div>
+        <h1 style={this.state.color}>{this.state.message}</h1>
+        <button onClick={this.onClickEnter}>입장</button>
+        <button onClick={this.onClickLeave}>퇴장</button>
+        <button onClick={this.handleColor}>글자색 변경</button>
+      </div>
+    )
+  }
+  }
+
