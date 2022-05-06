12dec8f8908b7d905cb226427c056144ccfc8ba2
gwangmiiin <0424kgm@gmail.com>
2022-05-06 19:51:49 +0900

    쇼핑몰 완성 +상세이미지 +로딩

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -4,15 +4,17 @@ import styles from '../styles/Home.module.css'
 import Axios from 'axios'
 import { useEffect, useState } from 'react'
 import ItemList from '../src/component/ItemList'
-import { Divider, Header } from 'semantic-ui-react'
+import { Divider, Header, Loader } from 'semantic-ui-react'
 
 export default function Home() {
   const [list, setList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   const API_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
   function getData() {
     Axios.get(API_URL).then((res) => {
       console.log(res.data)
       setList(res.data)
+      setIsLoading(false)
     })
   }
 
@@ -25,6 +27,15 @@ export default function Home() {
       <Head>
         <title>화장품 사이트</title>
       </Head>
+      {isLoading && (
+        <div style={{padding : '300px 0'}}>
+            <Loader inline = "centered" active >
+              Loading
+            </Loader>
+        </div>
+      )}
+      {!isLoading && (
+      <>
       <Header as = 'h3' style = {{paddingTop : 40}} > 베스트 상품 </Header>
       <Divider />
       <ItemList list = {list.slice(0, 9)}/>
@@ -32,6 +43,9 @@ export default function Home() {
       <Header as = 'h3' style = {{paddingTop : 40}} >  신상품 </Header>
       <Divider />
       <ItemList list = {list.slice(9)}/>
+      </>
+      )}
     </div>
+
   )
 }
--- nextjs-tutorial/pages/view/[id].js
+++ nextjs-tutorial/pages/view/[id].js
@@ -2,17 +2,22 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import Axios from 'axios'
 import Item from '../../src/component/Item'
+import { Loader } from 'semantic-ui-react'
 
 const Post = () => {
   const router = useRouter()
   const { id } = router.query
 
   const [item, setItem] = useState({})
-  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products.json?/id=${id}`
+  const [isLoading, setIsLoading] = useState(true)
+  console.log(id)
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`
 
   function getData() {
     Axios.get(API_URL).then((res) => {
-      console.log(res.data)
+      console.log('data', res.data)
+      setItem(res.data)
+      setIsLoading(false)
     })
   }
 
@@ -20,9 +25,23 @@ const Post = () => {
     if (id && id > 0) {
       getData()
     }
-  },[])
+  },[id])
 
-  return <Item item = {item}/>
+  return (
+    <>
+    {isLoading && (
+        <div style={{padding : '300px 0'}}>
+            <Loader inline = "centered" active >
+              Loading
+            </Loader>
+        </div>
+      )}
+  {
+    !isLoading && (
+  <Item item = {item}/>
+    )}
+   </>
+  )
 }
 
 export default Post
